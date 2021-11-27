b5b46463d068df7b177fbd72554b77ee840d8886
Hector <Hector>
2021-11-26 19:30:16 -0500

    proyecto angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BaseComponent } from './base/base.component';
+import { FooterrComponent } from './footerr/footerr.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseComponent,
+    FooterrComponent,
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
